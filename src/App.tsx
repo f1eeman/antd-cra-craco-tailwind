3556bdb14a7b104061b9c194fb5282e0a928d57b
f1eeman <mr.f1eeman@gmail.com>
2022-03-21 17:05:10 +0600

    feat(icons): add ant icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
-import {Button} from "antd";
+import {Layout, Menu} from "antd";
+import {
+    MenuUnfoldOutlined,
+    MenuFoldOutlined,
+    UserOutlined,
+    VideoCameraOutlined,
+    UploadOutlined,
+} from '@ant-design/icons';
+
+const { Header, Sider, Content } = Layout;
 
 function App() {
+    const [state, setState] = useState({
+        collapsed: false,
+    });
+
+    const toggle = () => {
+        setState((prev) => ({
+            collapsed: !prev.collapsed,
+        }));
+    };
   return (
-    <div className="App">
-      <Button type={'primary'}>SOME</Button>
-    </div>
+      <Layout>
+          <Sider trigger={null} collapsible collapsed={state.collapsed}>
+              <div className="logo" />
+              <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
+                  <Menu.Item key="1" icon={<UserOutlined />}>
+                      nav 1
+                  </Menu.Item>
+                  <Menu.Item key="2" icon={<VideoCameraOutlined />}>
+                      nav 2
+                  </Menu.Item>
+                  <Menu.Item key="3" icon={<UploadOutlined />}>
+                      nav 3
+                  </Menu.Item>
+              </Menu>
+          </Sider>
+          <Layout className="site-layout">
+              <Header className="site-layout-background" style={{ padding: 0 }}>
+                  {React.createElement(state.collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {
+                      className: 'trigger',
+                      onClick: toggle,
+                  })}
+              </Header>
+              <Content
+                  className="site-layout-background"
+                  style={{
+                      margin: '24px 16px',
+                      padding: 24,
+                      minHeight: 280,
+                  }}
+              >
+                  Content
+              </Content>
+          </Layout>
+      </Layout>
   );
 }
 
